8e6a7847ecc9879f894589bc272035df77f6a2db
YSW2 <jrgyber@gmail.com>
2026-04-30 16:15:33 +0900

    Feat: 타입 정의
    - 인증 관련 auth.ts
    - 서버 공통 응답 등 common.ts
    - 공공 의료 데이터 medicalStats.ts

---
+++ frontend/src/type/medicalStats.ts
@@ -0,0 +1,10 @@
+export type MedicalStats = {
+    명세서청구건수: Number;
+    '보험자부담금(선별포함)': Number;
+    '요양급여비용총액(선별포함)': Number;
+    의료기관종별: String;
+    입내원일수: Number;
+    '진료과목(표시과목)': String;
+    진료년도: Number;
+    환자수: Number;
+};
