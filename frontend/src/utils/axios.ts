21e29e0665195e26ae7eb6d12b92cb2e99bcec93
YSW2 <jrgyber@gmail.com>
2026-05-01 22:10:29 +0900

    Feat: axios 인터셉터 추가
    - jwt 토큰 헤더에 자동 포함

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -21,3 +21,14 @@ export const authClient = axios.create({
     },
     withCredentials: true,
 });
+
+authClient.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('auth_token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error),
+);
